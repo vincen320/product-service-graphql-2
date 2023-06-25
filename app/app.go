8720d7ebd577ef4af33c69d08dde70fe8cb8e3ac
Vincen <vincen320@gmail.com>
2023-06-26 00:35:15 +0700

    implements gracefully shutdown on server

--- app/app.go
+++ app/app.go
@@ -1,11 +1,17 @@
 package app
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/vincen320/product-service-graphql-2/middleware"
 	graphqlHandler "github.com/vincen320/product-service-graphql-2/modules/graphql/handler"
@@ -15,15 +21,20 @@ import (
 )
 
 func Run() {
-	var (
-		db                = NewDB()
-		productRepository = productRepository.NewProductRepository(db)
-		productUseCase    = productUseCase.NewProductUseCase(productRepository)
-	)
+	ctx, cancel := context.WithCancel(context.Background())
+	db := NewDB()
+	productRepository := productRepository.NewProductRepository(db)
+	productUseCase := productUseCase.NewProductUseCase(productRepository)
 	schemeFile, err := os.ReadFile("./modules/graphql/schema.graphql")
 	if err != nil {
 		panic(err)
 	}
+	go func() {
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+		<-stop
+		cancel()
+	}()
 	schemeString := string(schemeFile)
 	rootResolver := resolver.NewRootResolver(productUseCase)
 	schema := graphql.MustParseSchema(schemeString, rootResolver)
@@ -36,5 +47,22 @@ func Run() {
 	e := echo.New()
 	v1 := e.Group("v1")
 	v1.POST("/graphql", graphqlHandler.GraphQL, middleware.JWTAuthentication())
-	e.Start(":4000")
+	eg, egCtx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		err = e.Start(":4000")
+		log.Println("successfully gracefully shutdown server")
+		return err
+	})
+	eg.Go(func() error {
+		<-egCtx.Done()
+		err = db.Close()
+		log.Println("DB Successfully closed")
+		err = e.Shutdown(context.Background())
+		log.Println("HTTP server finish graceful shutdown")
+		return err
+	})
+	if err := eg.Wait(); err != nil {
+		log.Println("Exit Reason", err)
+	}
+	fmt.Println("Clean up")
 }
--- go.mod
+++ go.mod
@@ -26,4 +26,5 @@ require (
 	github.com/labstack/echo/v4 v4.10.2
 	github.com/lib/pq v1.10.9
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sync v0.3.0
 )
